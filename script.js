7e23d6f12325654e50716fca132c794e172c87ce
Federico Verón <fedeva96@gmail.com>
2024-05-16 13:37:57 -0400

    Agregado logica del juego

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Hola Mundo")
+let intentos = 6;
+let palabra = "APPLE";
+
+window.addEventListener('load', init)
+
+function init(){
+    console.log('Esto se ejecuta solo cuando se carga la pagina web')
+}
+
+const button = document.getElementById("guess-button");
+
+button.addEventListener("click", intentar);
+
+function intentar(){
+    const INTENTO = leerIntento();
+    INTENTO === palabra.toUpperCase() ? console.log("Correcto") : console.log("Incorrecto");
+}
+
+function leerIntento(){
+    let intento = document.getElementById("guess-input");
+    intento = intento.value;
+    intento = intento.toUpperCase(); 
+    return intento;
+}
