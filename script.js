136fe24705c90bac502b5f103fa694cdca357b33
Federico Verón <fedeva96@gmail.com>
2024-07-09 12:16:39 -0400

    mejorado el wordle con la clase nueva

--- script.js
+++ script.js
@@ -1,24 +1,112 @@
-let intentos = 6;
-let palabra = "APPLE";
+const INPUT = document.getElementById("guess-input");
+const BUTTON = document.getElementById("guess-button");
+const GRID = document.getElementById("grid");
+const RESTART = document.getElementById("restart-button");
+let intentos = 5;
+let PALABRA;
+let palabraIngresada;
 
-window.addEventListener('load', init)
+BUTTON.disable = true;
+RESTART.disabled = true;
 
-function init(){
-    console.log('Esto se ejecuta solo cuando se carga la pagina web')
+traerPalabra();
+
+INPUT.addEventListener("input", () => {
+  const inputLength = INPUT.value.length;
+  console.log(inputLength);
+
+  if (
+    inputLength < PALABRA.length ||
+    inputLength > PALABRA.length ||
+    inputLength === 0
+  ) {
+    BUTTON.disabled = true;
+  } else if (intentos === 0) {
+    INPUT.disabled = true;
+    BUTTON.disabled = false;
+  } else {
+    BUTTON.disabled = false;
+  }
+});
+
+BUTTON.addEventListener("click", intentar);
+
+function intentar() {
+  palabraIngresada = INPUT.value;
+  palabraIngresada = palabraIngresada.toUpperCase();
+
+  console.log(palabraIngresada);
+
+  if (intentos > 0) {
+    intentarPalabra(palabraIngresada);
+    verificarPalabra(palabraIngresada);
+  } else {
+    BUTTON.disabled = true;
+  }
 }
 
-const button = document.getElementById("guess-button");
-
-button.addEventListener("click", intentar);
-
-function intentar(){
-    const INTENTO = leerIntento();
-    INTENTO === palabra.toUpperCase() ? console.log("Correcto") : console.log("Incorrecto");
+function intentarPalabra(palabra) {
+  const ROW = document.createElement("div");
+  ROW.classList.add("row");
+  GRID.appendChild(ROW);
+  for (let i = 0; i < palabra.length; i++) {
+    const LETRA = document.createElement("span");
+    LETRA.classList.add("letter");
+    LETRA.setAttribute("style", `--i ${i}`);
+    LETRA.textContent = palabra[i];
+    if (palabra[i] === PALABRA[i]) {
+      LETRA.classList.add("correct");
+    } else if (PALABRA.includes(palabra[i])) {
+      LETRA.classList.add("exist");
+    } else {
+      LETRA.classList.add("incorrect");
+    }
+    ROW.appendChild(LETRA);
+    INPUT.value = "";
+    BUTTON.disabled = true;
+  }
+  intentos--;
 }
 
-function leerIntento(){
-    let intento = document.getElementById("guess-input");
-    intento = intento.value;
-    intento = intento.toUpperCase(); 
-    return intento;
+function verificarPalabra(palabra) {
+  if (palabra === PALABRA) {
+    const GANASTE = document.createElement("h1");
+    GANASTE.textContent = "¡Ganaste!";
+    GRID.appendChild(GANASTE);
+    intentos = 0;
+    INPUT.disabled = true;
+    RESTART.disabled = false;
+  } else if (intentos === 0) {
+    const PERDISTE = document.createElement("h1");
+    PERDISTE.textContent = "Perdiste...";
+    GRID.appendChild(PERDISTE);
+    INPUT.disabled = true;
+    RESTART.disabled = false;
+
+    console.log(PALABRA);
+  }
+}
+
+RESTART.addEventListener("click", reiniciar);
+
+function reiniciar() {
+  GRID.innerHTML = "";
+  intentos = 5;
+  INPUT.disabled = false;
+  BUTTON.disabled = false;
+  RESTART.disabled = true;
+}
+
+function traerPalabra() {
+  fetch("https://random-word-api.herokuapp.com/word?lang=es&length=5")
+    .then((respuesta) => respuesta.json())
+    .then((palabra) => setPalabra(palabra));
+}
+
+function setPalabra(palabra) {
+  PALABRA = palabra[0].toUpperCase();
+  INPUT.setAttribute(
+    "placeholder",
+    `Ingresa tu palabra de ${PALABRA.length} letras`
+  );
 }
